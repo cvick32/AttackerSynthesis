fb73830041099bd74405ae84b4cddb73217a10bc
cole <cole.d.vick@gmail.com>
2020-06-16 12:51:27 -0500

    first pass at korg redesign

--- KORG/korg.py
+++ KORG/korg.py
@@ -1,82 +1,56 @@
 from problem import Problem
 from glob import glob
-from cli import parse_args
-
-"""
-
-"""
-
-
-def main():
-    problem = parse_args()
-    return solve(problem)
+from cli import parse_args, clean_and_exit
+from helpers import coalesce, clean_up
+from spin import run_spin, make_all_trails, generate_trail_commands, parse_all_trails
+from attacker import Attacker, make_attackers, write_attackers
 
 
-def solve(p):
+def solve_attacker_syn(p):
     """
 	Attempts to solve the attacker synthesis problem given the problem definition
 	find attackers against a given model. The attacker 
 	is successful if the given phi is violated. The phi is initially 
 	evaluated by being composed with Q. 
-	@param p.P        : a promela model 
+	@param p.P            : a promela model 
 	@param p.phi          : LTL property satisfied by model || Q
 	@param p.Q            : a promela model
 	@param p.IO           : Input Output interface of Q's communication channels
 	@param p.max_attacks  : how many attackers to generate
-	@param p.recovery_huh : should the attackers be with_recovery?
+	@param p.recovery_huh : should the attackers recover?
 	@param p.name         : name of the files
 	@param p.characterize_huh : do you want us to characterize attackers after 
 						  producing them?
 	"""
+    p.make_daisy()
+    p.write_daisy()
+    p.make_phi()
 
-    # The name of the file we use to check that (model, (Q), phi) has a
-    # with_recovery attacker
-    with_recovery_phi_name = p.name + "_with_recovery_phi.pml"
-    # The subdirectory of out/ where we write our results
-    attacker_name = p.name + "_" + str(p.recovery_huh)
-    # The name of the file we use to check that model || daisy(Q) |/= phi
-    daisy_models_name = p.name + "_daisy_check.pml"
+    problem_file = coalesce([p.P, p.phi, p.daisy_name], p.daisy_models_name)
 
-    p.IO = sorted(list(p.IO))  # sorted list of events
-    # Make daisy attacker
-    net, label = makeDaisy(IO, Q, with_recovery, daisy_name)
-    daisy_string = makeDaisyWithEvents(IO, with_recovery, net, label)
-    writeDaisyToFile(daisy_string, daisy_name)
+    if not run_spin(problem_file):
+        p.print_no_solution()
+        clean_and_exit()
 
-    if with_recovery == False:
-        daisyPhi = phi
-    else:
-        daisyPhiString = makeDaisyPhiFinite(label, phi)
-        with open(with_recovery_phi_name, "w") as fw:
-            fw.write(daisyPhiString)
-        daisyPhi = with_recovery_phi_name
+    make_all_trails(problem_file, p.max_attacks)
+    trail_cmds = generate_trail_commands(p.daisy_models_name)
+    trails = parse_all_trails(trail_cmds, p.recovery_huh)
+    attackers = make_attackers(trails, trail_cmds, p)
 
-    # model, phi, N, name
-    _models = models(model, daisyPhi, daisy_name, daisy_models_name)
-
-    if net == None or _models:
-        printNoSolution(model, phi, Q, with_recovery)
-        cleanUp()
-        return 6
-
-    makeAllTrails(daisy_models_name, max_attacks)
-    # second arg is max# attacks to make
-
-    cmds = trailParseCMDs(daisy_models_name)
-    attacks, provenance = parseAllTrails(cmds, with_recovery)
-
-    # Write these attacks to models
-    writeAttacks(attacks, provenance, net, with_recovery, attacker_name)
-
+    write_attackers(attackers)
+    clean_up()
+    '''
     # Characterize the attacks
-    if characterize:
+    if p.characterize_huh:
         (E, A) = characterizeAttacks(model, phi, with_recovery, attacker_name)
-        cleanUp()
+        clean_up()
         return 0 if (E + A) > 0 else -1
     else:
-        cleanUp()
-        return 0  # assume it worked if not asked to prove it ...
+        '''
+        
+    return 0  # assume it worked if not asked to prove it ...
 
 
 if __name__ == "__main__":
-    main()
+    problem = parse_args()
+    solve_attacker_syn(problem)
